5705163c2ee312bbf0c188ac4e6924f0b3c0f7de
magfelz18@gmail.com <magfelz18@gmail.com>
2026-06-25 14:25:05 +0530

    Add deployment files

--- examportal/settings.py
+++ examportal/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = ['*']
